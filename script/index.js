778e1474b5e80dfa8485bc5c614892b8c1a53210
gusehd1481-droid <gusehd1481@gmail.com>
2026-04-13 18:22:42 +0900

    26/04/13학원 베스트셀러 진입

--- script/index.js
+++ script/index.js
@@ -2,15 +2,11 @@ const heroBnr = document.querySelector('.hero_bnr');
 console.log(heroBnr);
 const heroSwiper = new Swiper(heroBnr,{
     autoplay:{
-        delay:3000,
+        delay:4000,
     },
-    speed:2000,
+    effect:'fade',
     navigation:{
         prevEl:'.hero_bnr .prev',
         nextEl:'.hero_bnr .next',
-    },
-    pagination:{
-        el:'.hero_bnr .swiper-pagination',
-        type:'progressbar',
-    },
+    }
 });
