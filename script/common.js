e0b7bbe060ae0a585879a411cc9ce1fda2eee677
gusehd1481-droid <gusehd1481@gmail.com>
2026-04-15 18:16:18 +0900

    26/04/15학원

---
+++ script/common.js
@@ -0,0 +1,5 @@
+const header = document.querySelector('header');
+console.log(header);
+header.addEventListener('scroll',function(){
+    header.style.backgroundColor = '#fff'
+})
--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
 const heroBnr = document.querySelector('.hero_bnr');
-const bestSeller = document.querySelector('.best_swiper');
-const newProduct = document.querySelector('.new_swiper');
-console.log(heroBnr,bestSeller,newProduct);
+const bestToner = document.querySelector('.toner .best_toner_swiper');
+const bestAmpoule = document.querySelector('.ampoule .best_ampoule_swiper');
+console.log(heroBnr,bestToner,bestAmpoule);
 const heroSwiper = new Swiper(heroBnr,{
     autoplay:{
         delay:4000,
@@ -12,17 +12,37 @@ const heroSwiper = new Swiper(heroBnr,{
         nextEl:'.hero_bnr .next',
     }
 });
-const bestSwiper = new Swiper(bestSeller,{
+const bestTonerSwiper = new Swiper(bestToner,{
     slidesPerView:5,
     spaceBetween:20,
     scrollbar:{
-        el:'#best_wrap .product_slide .swiper-scrollbar',
+        el:'#best_wrap .toner .swiper-scrollbar',
     },
 });
-const newSwiper = new Swiper(newProduct,{
+const bestAmpouleSwiper = new Swiper(bestAmpoule,{
     slidesPerView:5,
     spaceBetween:20,
     scrollbar:{
-        el:'#new_wrap .product_slide .swiper-scrollbar',
+        el:'#best_wrap .ampoule .swiper-scrollbar',
     },
 });
+
+
+
+//===================================================================상품 슬라이드 product_slide 
+
+const bestProductSlide = document.querySelectorAll('#best_wrap .product_slide');
+const category = document.querySelectorAll('#best_wrap .category a');
+console.log(bestProductSlide,category);
+for(let i of category){
+    i.addEventListener('click',function(){
+        i.classList.remove('active');
+        i.classList.add('active');
+        productSlideHide();
+        bestProductSlide.style.display = 'block';
+    })
+};
+function productSlideHide(){
+    bestProductSlide.style.display = 'none';
+}
+//집에서 다시 ㄱ
